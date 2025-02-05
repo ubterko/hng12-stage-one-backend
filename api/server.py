57710e8c82d2cd65a90a644b4d396e9171112cd6
Israel Ebonko <ebonko2017ip@gmil.com>
2025-02-05 16:11:28 +0100

    bug fix

--- api/server.py
+++ api/server.py
@@ -4,14 +4,15 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_restful import Api, Resource
 
-# Initialize flask and flask extensions
+# Initialize flask
 app = Flask(__name__)
+
+# flask extensions
 api = Api(app)
 cors = CORS(app)
 
-# Returns a True or False depending on if the number is a prime number
+# Returns a True if number is a prime number.
 def is_prime(number):
-    # Conditions for number to be prime
     if (number < 2):
         return False
     if (number == 2):
@@ -23,12 +24,12 @@ def is_prime(number):
     for i in range(3, int(root_of_number)):
         # Check only odd numbers
         if (number % 2 != 0):
-            # If number is divisible by any return False
+            # If divisible by any it's not prime.
             if (number % i == 0) :
                 return False 
     return True
-    
-# Returns True or False depending on if number is perfect.
+
+# Returns true if number is perfect.
 def is_perfect(number):
     sum = 0
     for i in range(1, (number-1)):
@@ -39,21 +40,20 @@ def is_perfect(number):
         return True 
     return False
 
-# Checks if number is armstrong number
+# Returns true if armstrong number
 def is_armstrong(number):
     sum_of_power = 0
-    number_string = number.replace("-","")
-    number_of_digits = len(number_string) 
+    number_of_digits = len(number) 
     for index in range(number_of_digits):
-        sum_of_power += int(number_string[index]) ** number_of_digits
-    if (sum_of_power == int(number_string)):
+        sum_of_power += int(number[index]) ** number_of_digits
+    if (sum_of_power == int(number)):
         return True 
     return False
 
-# Returns an array containing properties of the number.
+# Returns an array containing properties of a number.
 def get_properties(number):
     properties = []
-    if is_armstrong(str(number)):
+    if is_armstrong(str(abs(number))):
         properties.append("armstrong")
     if (number % 2 == 0):
         properties.append("even")
@@ -63,7 +63,7 @@ def get_properties(number):
 
 # Returns the sum of digits in a number
 def sum_digits(number):
-    number = number.replace("-","")
+    number = str(abs(number))
     digit_sum = sum(int(digit) for digit in number)
     return digit_sum
         
@@ -72,12 +72,12 @@ class Number(Resource):
     def get(self):
         number = request.args.get("number")
         try:
-            concat_number = int(number)
-            is_prime_number = is_prime(concat_number)
-            is_perfect_number = is_perfect(concat_number)
-            numbers_properties = get_properties(concat_number)
+            number = int(number)
+            is_prime_number = is_prime(number)
+            is_perfect_number = is_perfect(number)
+            numbers_properties = get_properties(number)
             numbers_digit_sum = sum_digits(number)
-            response = requests.get(f"http://numbersapi.com/{concat_number}/math")
+            response = requests.get(f"http://numbersapi.com/{number}/math")
             numbers_fun_fact = response.text
         
             return {
